626f24e724ef30df16d2bf6f86d618c2d45b3f5f
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-06-04 08:51:38 -0500

    feat: add connection to solana blockchain using phantom wallet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ walletAddress, setWalletAddress ] = useState('');
+
+  useEffect(() => {
+    const onLoad = async () => setWalletAddress(await getWalletAddress(window.solana));
+    window.addEventListener('load', onLoad);
+
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  async function connectWallet() {
+    const { solana } = window;
+    if (window?.solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+        <h1>My first dapp with Solana</h1>
       </header>
+      <main>
+        {!walletAddress && <button className='cta-button connect-wallet-button' onClick={connectWallet}>Connect to wallet</button>}
+
+        {walletAddress && `Your Address: ${walletAddress}`}
+      </main>
+      <footer></footer>
     </div>
   );
+}
+
+async function getWalletAddress(solana) {
+  try {
+    if(hasSolanaWallet()) {
+      const response = await solana.connect({ onlyIfTrusted: true });
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      return response.publicKey.toString();
+    }
+
+    return '';
+  } catch(err) {
+    console.error(err);
+    return '';
+  }
+}
+
+async function hasSolanaWallet() {
+  const { solana } = window;
+
+  if (solana?.isPhantom) {
+    return true;
+  } else {
+   return false;
+  }
 }
 
 export default App;
